46c3e4106f939be3820ebedba94986ae811e794c
navneet0693 <navneetsingh0693@gmail.com>
2022-11-27 13:32:39 +0530

    Added functionality to limit field_registered_users as per slot booking availability on node add/edit form.
    1. Added a new class CovidCenterNodeForm which extends NodeForm for customizing the add/edit forms.
    2. Added validation on field_registered_users for not exceeding the slot availability in edit operations.
    3. Disabled slot booking field changes in edit mode.
    4. Added hook_entity_type_alter to replace the NodeForm with CovidCenterNodeForm.
    5. CovidCenterNodeForm was added so that we can do customizations it more object oriented rather than procedurale way of form alter.
    6. CovidCenterNodeForm location is in similar names space as per NodeForm.

---
+++ web/modules/custom/slot_booking_customizations/src/CovidCenterNodeForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\slot_booking_customizations;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\NodeForm;
+
+/**
+ * Form handler for the node edit forms.
+ */
+class CovidCenterNodeForm extends NodeForm {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+    // We want to restrict the customizations only if the vaccination center
+    // information is being altered/edited.
+    if ($this->operation == 'edit' && $this->entity->bundle() == 'covid_center') {
+      // Disable the available slots text field, we don't want to allow
+      // the user to alter the available slots information as it should
+      // added only at the time creating the covid center.
+      $form['field_available_slots']['widget'][0]['#disabled'] = 'TRUE';
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    if ($this->entity->bundle() == 'covid_center') {
+      // Get the available slots for this covid center.
+      $available_slots = $form_state->getValue('field_available_slots')[0]['value'];
+      // Count the number of registered users as of now.
+      $count = count(array_column($form_state->getValue('field_registered_users'), 'target_id'));
+      // Even though we have removed the 'add more' button in self::form() above,
+      // we want to check if the registered users are more than available slot.
+      // If it is, we want to throw an error.
+      if ($count > $available_slots) {
+        $form_state->setErrorByName('field_registered_users',
+          $this->t('Only @count are allowed to register in this vaccination center as per available slots.', [
+            '@count' => $available_slots,
+          ]));
+      }
+    }
+    return $this->buildEntity($form, $form_state);
+  }
+
+}
