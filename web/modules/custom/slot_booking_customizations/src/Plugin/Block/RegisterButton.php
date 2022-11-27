5fb5cf9978f213e244376dc1301c865d22340381
navneet0693 <navneetsingh0693@gmail.com>
2022-11-27 19:11:07 +0530

    BONUS: Changed the caching strategy on the RegisterButton block plugin.
    1. Added getCacheTags() function and added current nodes and current users tags. This will make sure to revalidate the cache of the block when some changes in the current node or current user.
    
    2. Added 'context_definitions' annotation in block and added 'user' and 'node' contexts. Read more at https://www.drupal.org/node/3016699
    
    3. Removed ContainerFactoryPluginInterface implementation as we can have the current user object and node object from getContextValue() function.
    
    4. Added a private function checkUserCity() to compare the current covid centers city and current user's city in order to present with 'Register' button on the covid center page, only if the match is successful.

--- web/modules/custom/slot_booking_customizations/src/Plugin/Block/RegisterButton.php
+++ web/modules/custom/slot_booking_customizations/src/Plugin/Block/RegisterButton.php
@@ -4,60 +4,31 @@ namespace Drupal\slot_booking_customizations\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Drupal\node\NodeInterface;
 use Drupal\user\UserInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a block with a button.
  *
  * @Block(
  *   id = "register_block",
- *   admin_label = @Translation("Register Block")
+ *   admin_label = @Translation("Register Block"),
+ *   context_definitions = {
+ *     "node" = @ContextDefinition(
+ *       "entity:node",
+ *       label = @Translation("Current Node")
+ *     ),
+ *     "user" = @ContextDefinition(
+ *       "entity:user",
+ *       label = @Translation("Current user")
+ *     ),
+ *   }
  * )
  */
-class RegisterButton extends BlockBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The current route.
-   *
-   * @var \Drupal\Core\Routing\CurrentRouteMatch
-   */
-  protected CurrentRouteMatch $currentRouteMatch;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected EntityTypeManagerInterface $entityTypeManager;
-
-  /**
-   * The current user.
-   *
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected AccountInterface $currentUser;
-
-  /**
-   * {@inheritDoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition
-    );
-    $instance->entityTypeManager = $container->get('entity_type.manager');
-    $instance->currentUser = $container->get('current_user');
-    $instance->currentRouteMatch = $container->get('current_route_match');
-    return $instance;
-  }
+class RegisterButton extends BlockBase {
 
   /**
    * @inheritDoc
@@ -70,7 +41,7 @@ class RegisterButton extends BlockBase implements ContainerFactoryPluginInterfac
       // This will not appear on the covid vaccination center outside
       // user's city.
       /** @var \Drupal\node\NodeInterface $node */
-      $node = $this->currentRouteMatch->getParameter('node');
+      $node = $this->getContextValue('node');
       return [
         '#markup' => $this->t('You are already registered in @node_title', [
           '@node_title' => $node->getTitle(),
@@ -78,32 +49,38 @@ class RegisterButton extends BlockBase implements ContainerFactoryPluginInterfac
       ];
     }
     else {
-      // User is not registered and present it with a registration link.
-      // We build the AJAX link.
-      $build['ajax_link']['link'] = [
-        '#type' => 'link',
-        '#title' => $this->t('Register'),
-        // We have to ensure that Drupal's Ajax system is loaded.
-        '#attached' => ['library' => ['core/drupal.ajax']],
-        // We add the 'use-ajax' class so that Drupal's AJAX system can spring
-        // into action.
-        '#attributes' => [
-          'class' => ['use-ajax button'],
-          'id' => ['register-button-div'],
-        ],
-        // The URL for this link element is the route for our controller to
-        // update users.
-        '#url' => Url::fromRoute('slot_booking_customizations.register'),
-      ];
-      return $build;
+      // User can only register in it's own city.
+      // Check if the current covid centers city is same as current user city.
+      if ($this->checkUserCity()) {
+        // User is not registered and present it with a registration link.
+        // We build the AJAX link.
+        $build['ajax_link']['link'] = [
+          '#type' => 'link',
+          '#title' => $this->t('Register'),
+          // We have to ensure that Drupal's Ajax system is loaded.
+          '#attached' => ['library' => ['core/drupal.ajax']],
+          // We add the 'use-ajax' class so that Drupal's AJAX system can spring
+          // into action.
+          '#attributes' => [
+            'class' => ['use-ajax button'],
+            'id' => ['register-button-div'],
+          ],
+          // The URL for this link element is the route for our controller to
+          // update users.
+          '#url' => Url::fromRoute('slot_booking_customizations.register'),
+        ];
+        return $build;
+      }
     }
+
+    return [];
   }
 
   /**
    * {@inheritdoc}
    */
   protected function blockAccess(AccountInterface $account) {
-    return AccessResult::allowedIfHasPermission($account, 'access content');
+    return AccessResult::allowedIf($account->isAuthenticated());
   }
 
   /**
@@ -116,36 +93,98 @@ class RegisterButton extends BlockBase implements ContainerFactoryPluginInterfac
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
   private function checkStatus(): bool {
+    // Get the user object from context.
+    $user = $this->getContextValue('user');
     // Check if the current user is authenticated.
-    if ($this->currentUser->isAuthenticated()) {
-      // Get the user object from current user.
-      $user = $this->entityTypeManager->getStorage('user')->load($this->currentUser->id());
-      // Proceed only if we get the genuine user entity.
-      if ($user instanceof UserInterface) {
-        // Get the current route object.
-        $node = $this->currentRouteMatch->getParameter('node');
-        // Check if the route object is a valid Node object.
-        // Only proceed if the node bundle is 'covid_center' and
-        // current users has the registered.
-        if ($node instanceof NodeInterface
-          && ($node->bundle() === 'covid_center')
-          && !$user->get('field_covid_center')->isEmpty()
+    // Proceed only if we get the genuine user entity.
+    if ($user instanceof UserInterface) {
+      // Get the node object.
+      $node = $this->getContextValue('node');
+      // Check if the route object is a valid Node object.
+      // Only proceed if the node bundle is 'covid_center' and
+      // current users has the registered.
+      if ($node instanceof NodeInterface
+        && ($node->bundle() === 'covid_center')
+        && !$user->get('field_covid_center')->isEmpty()
+      ) {
+        // Match the node ID in user entity's field_covid_center
+        // and current node objects ID.
+        // If they match, it means the current user is registered on the same
+        // vaccination center.
+        $value = $user->get('field_covid_center')->getValue()[0]['target_id'];
+        if (!empty($value)
+          && $value == $node->id()
         ) {
-          // Match the node ID in user entity's field_covid_center
-          // and current node objects ID.
-          // If they match, it means the current user is registered on the same
-          // vaccination center.
-          $value = $user->get('field_covid_center')->getValue()[0]['target_id'];
-          if (!empty($value)
-            && $value == $node->id()
-          ) {
-            return TRUE;
-          }
+          return TRUE;
+        }
+      }
+    }
+
+    // Otherwise, user has not registered.
+    return FALSE;
+  }
+
+  /**
+   * Matches the covid center's and user's city.
+   *
+   * @return bool
+   *   TRUE or FALSE depending upon the results of the match.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\ContextException
+   */
+  private function checkUserCity(): bool {
+    // Get the user object from context.
+    $user = $this->getContextValue('user');
+    // Check if the current user is authenticated.
+    // Proceed only if we get the genuine user entity.
+    if ($user instanceof UserInterface) {
+      // Get the node object.
+      $node = $this->getContextValue('node');
+      // Check if the route object is a valid Node object.
+      // Only proceed if the node bundle is 'covid_center' and
+      // current users has the registered.
+      if ($node instanceof NodeInterface
+        && ($node->bundle() === 'covid_center')
+        && ($node->hasField('field_tags'))
+        && !($node->get('field_tags')->isEmpty())
+        && !$user->get('field_city')->isEmpty()
+      ) {
+        // Match the covid center city and user's city.
+        // If they match, it means the current user is can register on the
+        // vaccination center.
+        $covid_center_city_tid = $node->get('field_tags')->getValue()[0]['target_id'];
+        $user_city_tid = $user->get('field_city')->getValue()[0]['target_id'];
+
+        if ($covid_center_city_tid == $user_city_tid) {
+          return TRUE;
         }
       }
     }
     // Otherwise, user has not registered.
     return FALSE;
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getCacheTags() {
+    // Our block should rebuild if the node changes.
+    if (($node = $this->getContextValue('node'))
+      && $node instanceof NodeInterface
+      && ($node->bundle() === 'covid_center')
+    ) {
+      $user = $this->getContextValue('user');
+      // Prepare the cache tags.
+      $cache_tags = [
+        'node:' . $node->id(),
+        'user:' . $user->id(),
+      ];
+      return Cache::mergeTags(parent::getCacheTags(), $cache_tags);
+    }
+
+    // Otherwise, return the default tags.
+    return parent::getCacheTags();
   }
 
 }
